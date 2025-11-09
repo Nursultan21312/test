e53424793e329479ef0065d51add69d019af8e7e
Nursultan21312 <ilihom0330@mail.ru>
2025-11-09 21:43:47 +0600

    2 4

--- test.py
+++ test.py
@@ -6,20 +6,11 @@ def main_page(page:ft.Page):
 
     greeting_history = []
     history_text = ft.Text('История приветствий')
-
+    like_list = []
+    like_text = ft.Text(value='избранные')
 
     page.theme_mode = ft.ThemeMode.LIGHT
 
-
-    with open("history.txt", "r") as file:
-        for line in file.readlines():
-            greeting_history.append(line)
-
-    
-    like_text = ft.Text("Избранные" + greeting_history[-1])
-    like_list = []
-    
-    
     def on_button_click(_):
         name = name_input.value.strip()
 
@@ -32,9 +23,15 @@ def main_page(page:ft.Page):
             greeting_text.value = f'Привет {name}'
             name_input.value = None
 
-            timestamp = datetime.now().strftime("%d:%m:%y" "%H:%M:%S" )
+            timestamp = datetime.now().strftime("%d:%m:%y  " "  %H:%M:%S" )
             greeting_history.append(f'{timestamp} {name}')
             history_text.value ='история приветсвий \n' + '\n'.join(greeting_history)
+
+
+            if len(greeting_history) >4:
+                greeting_history.pop(-5)
+            else:
+                None
         else:
             greeting_text.value = 'Введите возраст!'
             greeting_text.color = ft.Colors.RED    
@@ -62,14 +59,15 @@ def main_page(page:ft.Page):
         page.update()
 
     def on_click_like(_):
-        like_text.value = "Избранное \n" +greeting_history[-1]
-        page.update()
+            like_list.append(f'{greeting_history[-1]}')
+            like_text.value = 'избранные\n' + '\n'.join(like_list)
+            page.update()
 
 
     clear_button = ft.ElevatedButton(text='clear' , icon=ft.Icons.DELETE , on_click=clear_history)
-    like_button = ft.ElevatedButton("like", on_click=on_click_like)
+    like_button = ft.ElevatedButton("like",on_click=on_click_like)
 
-    page.add(greeting_text , name_input , input_button_text ,theme_mode_button, clear_button,like_button, history_text )
+    page.add(greeting_text , name_input , input_button_text ,theme_mode_button, clear_button,like_button, history_text , like_text  )
 
 
     
